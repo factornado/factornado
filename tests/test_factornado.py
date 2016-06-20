604aed2ed0508f3daecd57ccae538142d41da836
bibmartin <martin.journois@fr.michelin-solutions.com>
2016-06-20 20:38:53 +0200

    Fix pep8 issues

--- tests/test_factornado.py
+++ tests/test_factornado.py
@@ -2,12 +2,12 @@
 """
 Factornados Tests
 -----------------
-
 """
 
-import os
-import sys
-import factornado.service
+# import os
+# import sys
+# import factornado.service
+
 
 def test_true():
     assert True
