659dc3af4f6206e2c5cdd6fcf2f7eed5f58df603
David Talabard <david.talabard@live.fr>
2019-04-02 16:31:55 +0200

    User roles decorator

--- factornado/authentication.py
+++ factornado/authentication.py
@@ -4,22 +4,18 @@ import json
 from tornado import httpclient
 from jwt.algorithms import RSAAlgorithm
 
-def auth(handler_class):
+# Authentication data key
+AUTH_DATA = 'auth_data'
+
+def authenticated(handler_class):
     ''' Handle Tornado HTTP Bearer authentication using keycloak '''
     def wrap_execute(handler_execute):
-
-        def _unauthorized(handler):
-			handler.set_status(401)
-			handler.write("Unauthorized")
-			handler.finish()
-
-			return False
 
         def require_auth(handler, kwargs):
             # Check if bearer is present in authorization header
             header = handler.request.headers.get('Authorization')
             if header is None or not header.startswith('bearer '):
-                return _unauthorized(handler)
+                return _unauthorized(401, handler)
  
             # Retrieve JWK from server
             # JWK contains public key that is used for decode JWT token
@@ -35,16 +31,16 @@ def auth(handler_class):
                     jwk = json.loads(response.body.decode('utf-8'))
                     public_key = RSAAlgorithm.from_jwk(json.dumps(jwk['keys'][0]))
                     auth_data = jwt.decode(bearer, public_key, algorithms='RS256', options={'verify_aud': False})
+                    httpclient.HTTPClient().close()
                 else:
-                    return _unauthorized(handler)
-
-                httpclient.HTTPClient().close()
+                    httpclient.HTTPClient().close()
+                    return _unauthorized(401, handler)
 
             except jwt.ExpiredSignatureError:
-                return _unauthorized(handler)
+                return _unauthorized(401, handler)
 
             # Store connected authentication data in the handler
-            handler.request.headers.add('auth_data', auth_data)
+            handler.request.headers.add(AUTH_DATA, auth_data)
  
             return True
  
@@ -57,3 +53,49 @@ def auth(handler_class):
  
     handler_class._execute = wrap_execute(handler_class._execute)
     return handler_class
+
+def _unauthorized(code, handler):
+    ''' Return a HTTP error  '''
+    handler.set_status(code)
+    handler.write("Unauthorized" if code == 401 else "Forbidden" )
+    handler.finish()
+
+    return False
+
+def _checkRole(user_roles, roles):
+    ''' Check given role is inside or equals to user roles '''
+    if user_roles is None:
+        return False
+    # Roles is a list not a single element
+    if isinstance(roles, list):
+        found = False
+        for r in roles:
+            if r in user_roles:
+                found = True
+                break
+
+        return found == True
+ 
+    # Role is a single string
+    else:
+        return roles in user_roles
+ 
+    return False
+
+def roles(roles = None):
+    ''' Function decorator to check user role and allow access '''
+    def decorator(func):
+        def decorated(self, *args, **kwargs):
+            auth_data = self.request.headers.get(AUTH_DATA)
+ 
+            if auth_data is None:
+                return _unauthorized(401, self)
+ 
+            user_realm_roles = auth_data['realm_access']['roles']
+            # Check role if necessary
+            if roles is not None and _checkRole(user_realm_roles, roles) == False:
+                return _unauthorized(403, self)
+ 
+            return func(self, *args, **kwargs)
+        return decorated
+    return decorator
