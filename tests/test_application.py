cd32da2f3857bb9b704386ef509576f513ddb9ce
Martin Journois <martin@journois.fr>
2019-04-04 08:12:46 +0200

    Fix pep8

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,24 +1,34 @@
 import factornado
-from tornado import httpserver
+
+
+class Handler(factornado.RequestHandler):
+    def get(self):
+        self.write('This is GET')
+
+    def post(self):
+        self.write('This is POST')
+
+    def put(self):
+        self.write('This is PUT')
+
+
+app = factornado.Application(
+    {'name': 'test', 'threads_nb': 1, 'log': {'stdout': False}},
+    [('/', Handler)],
+    )
 
 
 def test_request():
-    class Handler(factornado.RequestHandler):
-        def get(self):
-            self.write('This is GET')
-
-        def post(self):
-            self.write('This is POST')
-
-        def put(self):
-            self.write('This is PUT')
-
-    app = factornado.Application(
-        {'name': 'test', 'threads_nb': 1, 'log': {'stdout': False}},
-        [('/', Handler)],
-        )
-
     assert app.request(method='GET', uri='/') == b'This is GET'
+
+
+def test_get():
     assert app.get('/') == b'This is GET'
+
+
+def test_post():
     assert app.post('/') == b'This is POST'
+
+
+def test_put():
     assert app.put('/') == b'This is PUT'
