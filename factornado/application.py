2cc80edb5fced1763461bf5d6b1bfc758747e4da
Martin Journois <BibMartin@users.noreply.github.com>
2019-04-05 17:40:53 +0200

    Add Application.request feature (#44)
    * Add Application.request feature
    
    * Fix pep8
    
    * Fix order bug in travis
    
    * Fix async issue in Application.request
    
    * Fix asyncio.run inexitence
    
    * Fix PEP8 again

--- factornado/application.py
+++ factornado/application.py
@@ -7,15 +7,50 @@ import socket
 import yaml
 import re
 import signal
+import asyncio
 
 import pymongo
 import requests
 import pandas as pd
-from tornado import ioloop, web, httpserver
+from tornado import ioloop, web, httpserver, iostream, http1connection, concurrent
 
 from factornado.logger import get_logger
 
 factornado_logger = logging.getLogger('factornado')
+
+
+async def _execute(self):
+    """Util function that helps builing Application.request method."""
+    # If template cache is disabled (usually in the debug mode),
+    # re-compile templates and reload static files on every
+    # request so you don't need to restart to see changes
+    if not self.application.settings.get("compiled_template_cache", True):
+        with web.RequestHandler._template_loader_lock:
+            for loader in web.RequestHandler._template_loaders.values():
+                loader.reset()
+    if not self.application.settings.get('static_hash_cache', True):
+        web.StaticFileHandler.reset()
+
+    self.handler = self.handler_class(self.application, self.request,
+                                      **self.handler_kwargs)
+    self.handler._auto_finish = False
+    transforms = [t(self.request) for t in self.application.transforms]
+
+    if self.stream_request_body:
+        self.handler._prepared_future = concurrent.Future()
+    # Note that if an exception escapes handler._execute it will be
+    # trapped in the Future it returns (which we are ignoring here,
+    # leaving it to be logged when the Future is GC'd).
+    # However, that shouldn't happen because _execute has a blanket
+    # except handler, and we cannot easily access the IOLoop here to
+    # call add_future (because of the requirement to remain compatible
+    # with WSGI)
+    await self.handler._execute(transforms, *self.path_args,
+                                **self.path_kwargs)
+    # If we are streaming the request body, then execute() is finished
+    # when the handler has prepared to receive the body.  If not,
+    # it doesn't matter when execute() finishes (so we return None)
+    return b''.join(self.handler._write_buffer)
 
 
 class Kwargs(object):
@@ -116,6 +151,62 @@ class Application(web.Application):
                 for subkey, subval in val.items()
                 })
             for key, val in self.config.get('services', {}).items()})
+
+    def request(self, **kwargs):
+        """Performs a request in the application without going through the network.
+
+        Parameters
+        ----------
+        **kwargs : see `httpserver.HTTPRequest` for details.
+        """
+        # Create a HTTPRequest corresponding to the request.
+        kwargs['connection'] = (
+            kwargs.get("connection")
+            or http1connection.HTTP1Connection(iostream.BaseIOStream(), True))
+        kwargs['server_connection'] = (
+            kwargs.get("server_connection")
+            or http1connection.HTTP1Connection(iostream.BaseIOStream(), False))
+        http_request = httpserver.HTTPRequest(**kwargs)
+
+        # Build the corresponding _HandlerDelegate.
+        handler = self.find_handler(http_request)
+
+        loop = asyncio.get_event_loop()
+        out = loop.run_until_complete(_execute(handler))
+        return out
+
+    def get(self, uri, **kwargs):
+        """Performs a GET request over the application, without going through the network.
+
+        Parameters
+        ----------
+        uri: str
+            The uri you want to query. For example '/toto?q=foo&p=bar'
+        **kwargs: see `httpserver.HTTPRequest` for details.
+        """
+        return self.request(method='GET', uri=uri, **kwargs)
+
+    def post(self, uri, **kwargs):
+        """Performs a POST request over the application, without going through the network.
+
+        Parameters
+        ----------
+        uri: str
+            The uri you want to query. For example '/toto?q=foo&p=bar'
+        **kwargs: see `httpserver.HTTPRequest` for details.
+        """
+        return self.request(method='POST', uri=uri, **kwargs)
+
+    def put(self, uri, **kwargs):
+        """Performs a PUT request over the application, without going through the network.
+
+        Parameters
+        ----------
+        uri: str
+            The uri you want to query. For example '/toto?q=foo&p=bar'
+        **kwargs: see `httpserver.HTTPRequest` for details.
+        """
+        return self.request(method='PUT', uri=uri, **kwargs)
 
     def get_port(self):
         if 'port' not in self.config:
